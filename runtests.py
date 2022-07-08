c2298018341751ccdeb32b0b49116a110fe26094
Genadij Razdorov <genadijrazdorov@gmail.com>
2022-07-08 10:01:30 +0200

    Fix runtests problem

--- runtests.py
+++ runtests.py
@@ -10,6 +10,8 @@ if __name__ == "__main__":
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
     django.setup()
     TestRunner = get_runner(settings)
-    test_runner = TestRunner()
+    test_runner = TestRunner(
+        verbosity=3,
+    )
     failures = test_runner.run_tests(["tests"])
     sys.exit(bool(failures))
--- src/django_hashtag/apps.py
+++ src/django_hashtag/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class DjangoHashtagConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'django_hashtag'
     verbose_name = 'Hashtag'
